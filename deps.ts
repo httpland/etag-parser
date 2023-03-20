ae8055b6bba688a6cc42d992753ad86c23557fbb
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-20 21:16:31 +0900

    feat(parse): add deserializer for entiry tag stream

---
+++ deps.ts
@@ -0,0 +1,4 @@
+// Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
+// This module is browser compatible.
+
+export { isString } from "https://deno.land/x/isx@1.0.0-beta.24/mod.ts";
--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
+export { parse } from "./parse.ts";
 export { stringify } from "./stringify.ts";
 export type {
   ETag,
