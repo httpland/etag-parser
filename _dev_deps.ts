fbaf5ed4367944e86ee446b1a0941f1ec563779b
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-26 23:27:16 +0900

    feat(deps): update deps version

--- _dev_deps.ts
+++ _dev_deps.ts
@@ -2,5 +2,5 @@ export {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.180.0/testing/asserts.ts";
-export { describe, it } from "https://deno.land/std@0.180.0/testing/bdd.ts";
+} from "https://deno.land/std@0.181.0/testing/asserts.ts";
+export { describe, it } from "https://deno.land/std@0.181.0/testing/bdd.ts";
--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-export { isString } from "https://deno.land/x/isx@1.0.0-beta.24/mod.ts";
+export { isString } from "https://deno.land/x/isx@1.0.0/is_string.ts";
